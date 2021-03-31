4d7346025cf91585ba0b692dee5cce5df28554f3
dxy <dxy@dxyatguigu.com>
2021-03-31 08:48:41 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author dxy
+ * @date 2021/3/31 8:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
