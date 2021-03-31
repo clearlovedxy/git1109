18358b46f7519e25cbf9dbd75122b6ba26be36af
clearlovedxy <81625955+clearlovedxy@users.noreply.github.com>
2021-03-30 17:27:11 -0800

    dxy github commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello hot-fix2");
         System.out.println("master test2");
         System.out.println("hello hot-fix3");
+        System.out.println("dxygithub");
     }
 }
